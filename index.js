e7edeff62ae8715d4a8eb721cf8266e9e57ab16a
Ynda Jas <yndajas@gmail.com>
2021-03-28 17:32:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-'\s]/g, '');
+  }
+
+  static titleize(string) {
+    // capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word
+    const lower = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const originalWords = string.split(" ");
+    const titleizedWords = originalWords.map((word, index) => {
+      if (index === 0) {
+        return this.capitalize(word);
+      } else if (lower.includes(word.toLowerCase())) {
+        return word.toLowerCase();
+      } else {
+        return this.capitalize(word);
+      }
+    });
+    return titleizedWords.join(" ");
+  }
 }
